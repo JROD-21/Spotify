1e7a851798c49957b74f5fd15cf2b929e99971d3
JROD-21 <joseph.f.rodas@gmail.com>
2022-10-19 18:16:59 -0400

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Developer J-ROD on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
